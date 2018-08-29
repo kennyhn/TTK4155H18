9a858341b90f5388cdabf740e9f5b936797e7cf5
Iver Myklebust <iverom@gmail.com>
2018-08-29 16:13:45 +0200

    square pulse program

---
+++ main.c
@@ -0,0 +1,19 @@
+#define F_CPU 4915200
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include <stdio.h>
+
+int main(){
+    DDRA =  0xFF; //Konfig port
+
+    while(1){
+        PORTA = 1;
+        _delay_ms(2);
+        PORTA = 0;
+        _delay_ms(2); 
+        
+    }
+    
+    return 0;
+}
